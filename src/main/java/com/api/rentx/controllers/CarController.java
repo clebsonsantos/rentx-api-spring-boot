c170d49915d1c9789af3725a1990c176c921159e
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-12-10 10:13:24 -0300

    remove imports

--- src/main/java/com/api/rentx/controllers/CarController.java
+++ src/main/java/com/api/rentx/controllers/CarController.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
